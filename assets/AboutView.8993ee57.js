5539a83fa547633aac5d6b3218335e788330d325
Mahesh Bansod <mahesh0bansod@gmail.com>
2023-05-18 19:05:33 +0530

    Deploy

---
+++ assets/AboutView.8993ee57.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as _,a as e,b as t}from"./index.f6a3b2e6.js";const c={},r={class:"about"},a=e("h1",null,[t("Yours truly (fr fr),"),e("br"),t(" Light ")],-1),n=[a];function i(u,d){return s(),_("div",r,n)}const f=o(c,[["render",i]]);export{f as default};
